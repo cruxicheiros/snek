411ccf43abf889d7fddb85ec4da0bf64b6756bcd
Hazel Seanor <avellana1111@gmail.com>
2017-10-26 04:12:55 +0100

    Fixed canvas issues caused by resizing the browser window. Line can also now be drawn behind the image - special thanks to @redsummernight

--- js/CanvasController.js
+++ js/CanvasController.js
@@ -1,7 +1,11 @@
 function initCanvas(canvas, ctx) {
-    canvas.addEventListener("resize", setCanvasDimensions, false);
-    canvas.addEventListener("mousemove", function (e) {update(e, ctx);}, false);
-    setCanvasDimensions(canvas)
+    window.addEventListener("resize", setCanvasDimensions, false);
+    document.addEventListener("mousemove", function (e) {
+                                            var mouseX = e.x * canvas.width / canvas.clientWidth | 0;
+                                            var mouseY = e.y * canvas.height / canvas.clientHeight | 0;
+                                            update(mouseX, mouseY, canvas, ctx);
+                                            }, false);
+    setCanvasDimensions(canvas);
 }
 
 function setCanvasDimensions(canvas) {
@@ -9,20 +13,20 @@ function setCanvasDimensions(canvas) {
     canvas.width = window.innerWidth;
 }
 
-function calculateLineEndpointX(mouseX){
-    if (mouseX > window.innerWidth/2) {
-        return window.innerWidth + 100;
+function calculateLineEndpointX(mouseX, canvas){
+    if (mouseX > canvas.width/2) {
+        return canvas.width + 100;
     }
     return -100;
 }
 
-function update(e, ctx) {
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+function update(x, y, canvas, ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.moveTo(calculateLineEndpointX(e.x), e.y);
-    ctx.lineTo(e.x, e.y);
+    ctx.moveTo(calculateLineEndpointX(x, canvas), y);
+    ctx.lineTo(x, y);
     ctx.lineWidth = 100;
     ctx.lineCap = 'round';
-    ctx.strokeStyle ='#0a0a0a';
+    ctx.strokeStyle = '#0a0a0a';
     ctx.stroke();
 }
